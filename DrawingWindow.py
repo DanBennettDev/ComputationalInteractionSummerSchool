af6ce9e07c69db43ebf99ebc7419898b39da1576
danBennettDev <skjolbrot@gmail.com>
2018-09-01 12:00:11 +0100

    final before presentation

--- DrawingWindow.py
+++ DrawingWindow.py
@@ -20,7 +20,9 @@ class DrawingWindow(object):
         self.drawing = False
         self.newData = False 
         self.closed = False
-        
+        self.newClick = False
+        self.lastClick = [0, 0]
+
 
         
     def setup(self):
@@ -50,10 +52,15 @@ class DrawingWindow(object):
         elif event == cv2.EVENT_LBUTTONUP:
             self.drawing = False
             cv2.circle(self.img,(x,y),5,(0,0,255),-1)
-            self.allLines.append(np.array(self.linebuffer))
+            # distinguish between click and draw
+            if len(self.linebuffer) > 10:
+                self.allLines.append(np.array(self.linebuffer))
+                self.newData = True
+            else:
+                self.lastClick = [x, y]
+                self.newClick = True
             # clear the window
             self.img = np.zeros((512,512,4), np.uint8)
-            self.newData = True
     
 
     def draw(self):
@@ -71,8 +78,30 @@ class DrawingWindow(object):
                 return 1
             return 0
         
-    def displayImg(self, img, x, y):
-        self.img[y:y+img.shape[0], x:x+img.shape[1]] = img
+        
+        
+    def displayImg(self, overlay, x, y, scale=1):
+#         self.img[y:y+overlay.shape[0], x:x+overlay.shape[1]] = overlay
+        
+        
+        overlay = cv2.resize(overlay,(0,0),fx=scale,fy=scale)
+        h,w,_ = overlay.shape  # Size of foreground
+        rows,cols,_ = self.img.shape  # Size of background Image
+#         y,x = pos[0],pos[1]    # Position of foreground/overlay image
+
+        #loop over all pixels and apply the blending equation
+        for i in range(h):
+            for j in range(w):
+                if x+i >= rows or y+j >= cols:
+                    continue
+                alpha = float(overlay[i][j][3]/255.0) # read the alpha channel 
+                self.img[x+i][y+j][:3] = alpha*overlay[i][j][:3]+(1-alpha)*self.img[x+i][y+j][:3]
+
+
+    def getClick(self):
+        retNewClick = self.newClick
+        self.newClick = False
+        return self.lastClick, retNewClick
 
     def clear(self):
         self.img = np.zeros((512,512,4), np.uint8)
